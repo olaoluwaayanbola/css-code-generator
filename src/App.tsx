6fe43b0e2a13602b4297496bdfb20678e8665d1b
Ayanbola <olaoluwaayanbola@gmail.com>
2022-10-11 20:22:08 +0100

    improved functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AppStyle from './App.module.css';
 import { Routes, Route } from "react-router-dom"
 import { Transform } from "./Pages/Transform/Transform"
+import { Gradient } from './Pages/Gradient/Gradient';
 import { BoxShadow } from "./Pages/BoxShadow/BoxShadow"
 import { LandingPage } from './Pages/LandingPage/LandingPage';
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/BoxShadow" element={<BoxShadow />} />
         <Route path="/Transform" element={<Transform  />} />
+        <Route path="/Gradient" element={<Gradient  />} />
       </Routes>
     </div>
   );
