067e54b9d1642f391a6ef2f40d807ebe99b86489
Ayanbola <olaoluwaayanbola@gmail.com>
2023-03-04 19:10:18 +0100

    major syntax update

--- src/Components/Controls/RGBAControls.tsx
+++ src/Components/Controls/RGBAControls.tsx
@@ -1,22 +1,21 @@
 import 'antd/dist/antd.css';
-import { useContext } from "react"
-import React, { useState } from 'react'
-import { ChromePicker } from 'react-color'
-import ControlStyle from "./Controls.module.scss"
-import { contextFirst } from "../../Context/Context"
-
+import { useContext } from 'react';
+import React, { useState } from 'react';
+import { ChromePicker } from 'react-color';
+import ControlStyle from './Controls.module.scss';
+import { contextFirst } from '../../Context/Context';
 
 export const RGBAControls = () => {
-  const boxContext = useContext<any>(contextFirst)
-  const [colortruth, setColortruth] = useState<boolean>(false)
-  const { color, setColor }: any = boxContext
+  const boxContext = useContext<any>(contextFirst);
+  const [colortruth, setColortruth] = useState<boolean>(false);
+  const { color, setColor }: any = boxContext;
 
   const handleColorChange = (color: any): void => {
-    setColor(color.hex)
-  }
+    setColor(color.hex);
+  };
   const handlecolordisplay = (): void => {
-    setColortruth(prev => !prev)
-  }
+    setColortruth((prev) => !prev);
+  };
   return (
     <div className={ControlStyle.ControlsContainer}>
       <div className={ControlStyle.ControlBox}>
@@ -25,15 +24,14 @@ export const RGBAControls = () => {
         </div>
         <div className={ControlStyle.Select}>
           <h3>Color</h3>
-          <div className={ControlStyle.colorConatainer} >
-            {
-              colortruth &&
+          <div className={ControlStyle.colorConatainer}>
+            {colortruth && (
               <ChromePicker
                 className={ControlStyle.Hexcolor}
                 color={color}
                 onChangeComplete={handleColorChange}
               />
-            }
+            )}
             <div
               className={ControlStyle.colorPicker}
               onClick={handlecolordisplay}
@@ -45,5 +43,5 @@ export const RGBAControls = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/Context/Context.tsx
+++ src/Context/Context.tsx
@@ -1,72 +1,74 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from 'react';
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 interface Form {
-    inset: String,
-    Horizontaloffset: number,
-    Verticaloffset: number,
-    Blur: number,
-    Spread: number,
+  inset: String;
+  Horizontaloffset: number;
+  Verticaloffset: number;
+  Blur: number;
+  Spread: number;
 }
 interface TransForm {
-    Scale: number,
-    Rotate: number,
-    TranslateX: number,
-    TranslateY: number,
-    SkewX: number,
-    SkewY: number,
+  Scale: number;
+  Rotate: number;
+  TranslateX: number;
+  TranslateY: number;
+  SkewX: number;
+  SkewY: number;
 }
 
-export const contextFirst: React.Context<any> = createContext({})
+export const contextFirst: React.Context<any> = createContext({});
 export const FirstContext = ({ children }: Props) => {
-    const [code, setCode] = useState<any[]>([])
-    const [handleSeleted, setSelected]: any = useState("No")
-    const [dark, setDark] = useState<boolean>(true)
-    const [color, setColor] = useState(`#b9b3b3`)
-    const [Handlecolors, SetColors] = useState({ background: '#7441e2' })
-    const [Handlecolorstwo, SetColorstwo] = useState({ background: '#823e3e' })
-    const [Handlecolorsthree, SetColorsthree] = useState<number>(75)
-    const [form, setForm] = useState<Form>({
-        inset: "No",
-        Horizontaloffset: 0,
-        Verticaloffset: 0,
-        Blur: 0,
-        Spread: 0,
-    })
-    const [TransForm, setTransForm] = useState<TransForm>({
-        Scale: 0.5,
-        Rotate: 0,
-        TranslateX: 0,
-        TranslateY: 0,
-        SkewX: 0,
-        SkewY: 0,
-    })
-    const trigger = (): void => {
-        setDark(prev => !prev)
-    }
-    return (
-        <contextFirst.Provider value={{
-            code,
-            setCode,
-            dark,
-            trigger,
-            form,
-            setForm,
-            color,
-            setColor,
-            TransForm,
-            setTransForm,
-            handleSeleted,
-            setSelected,
-            Handlecolors,
-            SetColors,
-            Handlecolorstwo,
-            SetColorstwo,
-            Handlecolorsthree,
-            SetColorsthree
-        }}>
-            {children}
-        </contextFirst.Provider>
-    )
-}
+  const [code, setCode] = useState<any[]>([]);
+  const [handleSeleted, setSelected]: any = useState('No');
+  const [dark, setDark] = useState<boolean>(true);
+  const [color, setColor] = useState(`#b9b3b3`);
+  const [Handlecolors, SetColors] = useState({ background: '#7441e2' });
+  const [Handlecolorstwo, SetColorstwo] = useState({ background: '#823e3e' });
+  const [Handlecolorsthree, SetColorsthree] = useState<number>(75);
+  const [form, setForm] = useState<Form>({
+    inset: 'No',
+    Horizontaloffset: 0,
+    Verticaloffset: 0,
+    Blur: 0,
+    Spread: 0,
+  });
+  const [TransForm, setTransForm] = useState<TransForm>({
+    Scale: 0.5,
+    Rotate: 0,
+    TranslateX: 0,
+    TranslateY: 0,
+    SkewX: 0,
+    SkewY: 0,
+  });
+  const trigger = (): void => {
+    setDark((prev) => !prev);
+  };
+  return (
+    <contextFirst.Provider
+      value={{
+        code,
+        setCode,
+        dark,
+        trigger,
+        form,
+        setForm,
+        color,
+        setColor,
+        TransForm,
+        setTransForm,
+        handleSeleted,
+        setSelected,
+        Handlecolors,
+        SetColors,
+        Handlecolorstwo,
+        SetColorstwo,
+        Handlecolorsthree,
+        SetColorsthree,
+      }}
+    >
+      {children}
+    </contextFirst.Provider>
+  );
+};
